6a4f1c8ee9264edbef64e2b4c0c4a324cc525961
Ina Botnaru <ibotnaru@e1z2r7p29.42.us.org>
2019-03-06 17:19:02 -0800

    back_up

---
+++ main.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibotnaru <ibotnaru@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/06 16:56:52 by ibotnaru          #+#    #+#             */
+/*   Updated: 2019/03/06 17:16:35 by ibotnaru         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int main ()
+{
+    int     fd;
+    char    *line;
+
+    fd = open("my_file.txt", O_RDONLY);
+    //check if the file is not going to be opened
+    if (fd < 0)     //if fd is -1(for errors) or 0(if empty)
+        return (-1);
+        //if the file is successfuly open
+    while (get_next_line(fd, &line) == 1)    //using & because we have a pointer *line | ==1 means if we have at least 1 character in out file
+    {
+        ft_putendl(line);      //print the line till end of the line(before \n)
+    }
+	close(fd);
+	return (0);	
+}
